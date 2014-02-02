7bbba9e2c4ec5914a4a953e78ba32d138e8e280f
MerioSol <meriosol@users.noreply.github.com>
2014-02-01 23:26:34 -0500

    Update clij.bat
    env fix

--- configs/javadb_sample_runtime/clij.bat
+++ configs/javadb_sample_runtime/clij.bat
@@ -6,6 +6,6 @@
 setLocal
 call _env
 @echo To exit from derby CLI, enter quit;
-%JAVA_HOME%\bin\java -cp %CLIENT_CPATH% %JAVA_OPTS% org.apache.derby.tools.ij
+%JAVA_HOME%\bin\java -cp %CLIENT_CPATH% %CLIENT_JAVA_OPTS% org.apache.derby.tools.ij
 
 endLocal
