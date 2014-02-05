2b69fa475387c473cdee417efd90e803d9c3eeb6
cahotester <meriosol89@reallymymail.com>
2014-02-05 00:34:07 -0500

    Cleanup

--- configs/sql_db_samples/mysql/sql/create_db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE eventium CHARACTER SET utf8 COLLATE utf8_general_ci;
-GRANT ALL ON eventium.* TO etracker@localhost IDENTIFIED BY 'shmetracker';
-FLUSH PRIVILEGES;
-SHOW DATABASES;
-  
--- configs/sql_db_samples/mysql/sql/drop_tables.sql
+++
@@ -1,4 +0,0 @@
--- Clean eventium DB.
--- Drop tables:		
-DROP TABLE events;		
-DROP TABLE event_categories;		
